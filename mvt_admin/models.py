f3b21d751da8366a969200246e3700d4ea344ef9
Martin Baillie <martin@martinbaillie.net>
2014-01-14 21:34:31 +0000

    gmail field added

--- mvt_admin/models.py
+++ mvt_admin/models.py
@@ -1,3 +1,6 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class UserExtension(models.Model):
+    user = models.OneToOneField(User)
+    gmail = models.CharField(max_length=200)
